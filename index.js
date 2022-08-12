c304c778566a62688e875f7622a90de66a4b69f5
KhodakovskaMarya <khodakovska.marya@gmail.com>
2022-08-12 22:51:46 +0300

    1_2_3

---
+++ index.js
@@ -0,0 +1,12 @@
+function areaOfARectangle (a,b){
+    let result = a*b;
+    return result;
+}
+function circumference (d) {
+    let result = 3.14 *d;
+    return result;
+}
+function volumeOfTheCylinder(h,d){
+    let result = ((3.14/4)*d**2)*h;
+    return result;
+}
